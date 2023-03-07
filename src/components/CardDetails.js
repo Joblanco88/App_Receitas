f22b9e7a00c2fb3171e4c8eac71aa2e4bbec6693
Luiz Henrique <wxluiz@gmail.com>
2023-03-07 17:25:30 -0300

    Recipe Details - Req 24 e 25

---
+++ src/components/CardDetails.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+export default function CardDetails(recipeId) {
+  const { params: { title, thumb, category,
+    ingredient, measure, instruction, video } } = recipeId;
+  console.log(ingredient);
+  return (
+    <div>
+      {/* titulo */}
+      <h1
+        data-testid="recipe-title"
+      >
+        {title}
+      </h1>
+      {/* foto */}
+      <img
+        data-testid="recipe-photo"
+        src={ thumb }
+        alt={ thumb }
+        width="300px"
+      />
+      {/* texto da categoria */}
+      <p
+        data-testid="recipe-category"
+      >
+        {category}
+      </p>
+      {/* // ingredientes */}
+      <ul>
+        {ingredient.map((el, index) => (
+          <li
+            data-testid={ `${index}-ingredient-name-and-measure` }
+            key={ index }
+          >
+            {`${el}-${measure[index]}`}
+          </li>))}
+      </ul>
+      {/* texto de instruções */}
+      <p
+        data-testid="instructions"
+      >
+        Modo de preparo:
+        <br />
+        {`${instruction}`}
+      </p>
+      <div>
+        {/* vídeo presente somente na tela de comidas */}
+        <iframe
+          title="video"
+          data-testid="video"
+          width="420"
+          height="345"
+          src={ video }
+        />
+      </div>
+    </div>
+  );
+}
+
+// "strDrinkThumb", "strDrink", "strCategory", "strIngredient1", "strMeasure1", "strInstrunctions", "strVideo"
--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,12 +1,74 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { idDrinkFetch, idMealFetch } from '../helpers/services/fetchAPI';
+import CardDetails from '../components/CardDetails';
 
 export default function RecipeDetails({ match }) {
+  const { params: { id } } = match;
+  const { path } = match;
+  const stateDefault = {
+    thumb: '',
+    title: '',
+    category: '',
+    ingredient: [''],
+    measure: [''],
+    instruction: '',
+    video: '',
+  };
+  const [recipeId, setRecipeId] = useState(stateDefault);
+  const PARAM_INGREDIENT = /strIngredient\d+/;
+  const PARAM_MEASURE = /strMeasure\d+/;
+
+  const filterKeys = (object, param) => {
+    const chavesFiltradas = {};
+    Object.keys(object).forEach((key) => {
+      if (param.test(key)) {
+        chavesFiltradas[key] = object[key];
+      }
+    });
+    return chavesFiltradas;
+  };
+
   useEffect(() => {
-    const { params: { id } } = match;
-  }, [match]);
+    const idFetchs = async () => {
+      if (path === '/meals/:id') {
+        const response = await idMealFetch(id);
+        const objectApi = response.meals[0];
+        const ingredients = filterKeys(objectApi, PARAM_INGREDIENT);
+        const measures = filterKeys(objectApi, PARAM_MEASURE);
+        const object = {
+          thumb: objectApi.strMealThumb,
+          title: objectApi.strMeal,
+          category: objectApi.strCategory,
+          ingredient: Object.values(ingredients).filter((value) => value !== ''),
+          measure: Object.values(measures).filter((value) => value !== ''),
+          instruction: objectApi.strInstructions,
+          video: objectApi.strYoutube,
+        };
+        setRecipeId(object);
+      } else if (path === '/drinks/:id') {
+        const response = await idDrinkFetch(id);
+        const objectApi = response.drinks[0];
+        const ingredients = filterKeys(objectApi, PARAM_INGREDIENT);
+        const measures = filterKeys(objectApi, PARAM_MEASURE);
+        const object = {
+          thumb: objectApi.strDrinkThumb,
+          title: objectApi.strDrink,
+          category: objectApi.strAlcoholic,
+          ingredient: Object.values(ingredients).filter((value) => value !== ''),
+          measure: Object.values(measures).filter((value) => value !== ''),
+          instruction: objectApi.strInstructions,
+          video: objectApi.strVideo,
+        };
+        setRecipeId(object);
+      }
+    };
+    idFetchs();
+  }, []);
   return (
-    <div>RecipeDetails</div>
+    <div>
+      <CardDetails params={ recipeId } />
+    </div>
   );
 }
 
@@ -17,3 +79,5 @@ RecipeDetails.propTypes = {
     }),
   }),
 }.isRequired;
+
+// "strDrinkThumb", "strDrink", "strCategory", "strIngredient1", "strMeasure1", "strInstrunctions", "strVideo"
